4032dbe7884d18dd8f7240b5fcec2067ba7512d0
AgesilausDrako <agesilaus@yahoo.com>
2017-10-07 18:47:33 -0400

    articles rendering in handlebars

--- server.js
+++ server.js
@@ -92,14 +92,14 @@ app.get("/scrape", function(req, res) {
 // // This will get the articles we scraped from the mongoDB
 app.get("/articles", function(req, res) {
   // Grab every doc in the Articles array
-  Article.find({}, function(error, doc) {
+  Article.find({}, function(error, data) {
     // Log any errors
     if (error) {
       console.log(error);
     }
     // Or send the doc to the browser as a json object
     else {
-      res.render("index", doc);
+      res.render("index", {data});
     }
   });
 });
